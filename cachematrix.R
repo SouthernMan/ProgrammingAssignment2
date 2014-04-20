bab513f118589bc5d5715700cff3069625f7851e
SouthernMan <brian.d.andrews@gmail.com>
2014-04-20 21:32:09 +0800

    Signed-off-by: SouthernMan <brian.d.andrews@gmail.com>

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,30 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        Matrix        <- x
+        MatrixInverse <- NULL
 
+        set <- function(y) {
+                Matrix        <<- y
+                MatrixInverse <<- NULL
+        }
+
+        get <- function() {
+                Matrix
+        }
+
+        setInverse <- function(Inverse) {
+                MatrixInverse <<- Inverse
+        }
+
+        getInverse <- function() {
+                 MatrixInverse
+        }
+
+        list(set        = set        , 
+             get        = get        ,
+             setInverse = setInverse ,
+             getInverse = getInverse   )
 }
 
 
@@ -12,4 +35,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        mi <- x$getInverse()
+        if(!is.null(mi)) {
+                message("getting cached inverse")
+                return(mi)
+        }
+        message("solving inverse")
+        data <- x$get()
+        mi <- solve(data, ...)
+        x$setInverse(mi)
+        mi
 }
